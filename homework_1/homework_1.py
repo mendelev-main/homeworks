ac2f31642d5a838aba22c1424f3765e86acf4ec6
Aleksandr <mendelev.basic@gmail.com>
2023-05-17 18:06:36 +0300

    new_

---
+++ homework_1/homework_1.py
@@ -0,0 +1,31 @@
+# 1. Написать функцию read_humans, которая принимает аргументом название csv-файла, и возвращает список
+# словарей (персонажей), у которых species это human
+
+# 2. Написать функцию to_json, которая принимает аргументом список словарей и название json-файла, и записывает
+# в файл список словарей в формате json (с индентацией в 4 пробела)
+
+# 3. Вызвать функцию read_humans, ее результат передать в функцию to_json
+
+import json
+import csv
+
+
+def read_humans(csv_file):
+    with open(csv_file, 'r') as file:
+        reader = csv.DictReader(file)
+        sort_species = []
+        for card_person in reader:
+            if card_person['Species'] == 'Human':
+                sort_species.append(card_person)
+        return sort_species
+
+
+print(read_humans('characters.csv'))
+
+
+def to_json(dictionary_list, file_name_json):
+    with open(file_name_json, 'w') as file:
+        json.dump(dictionary_list, file, indent=4)
+
+
+to_json(read_humans('characters.csv'), 'people.json')
