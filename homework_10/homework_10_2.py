106460a7b6e98a462f790907ae0218e6a30122cd
Aleksandr <mendelev.basic@gmail.com>
2023-05-29 14:21:00 +0300

    added homework_10_2

---
+++ homework_10/homework_10_2.py
@@ -0,0 +1,34 @@
+# *2. Написать программу, которая считывает машины из cars.json и десериализует их в объекты датакласса Car.
+# Нужно найти среднюю стоимость фордов мустангов. Обратите внимание, что некоторые записи не содержат информации о цене,
+# такие записи нужно пропускать.
+import dataclasses
+import json
+
+with open('cars.json', 'r', encoding='utf-8') as f:
+    text = json.load(f)
+
+
+@dataclasses.dataclass
+class Car:
+    make: str
+    model: str
+    year: int
+    price: int
+    color: str
+    mileage: int
+
+
+my_cars = []
+for i in text:
+    if i['make'] == 'Ford' and i['model'] == 'Mustang':
+        try:
+            car = Car(i['make'], i['model'], i['year'], i['price'], i['color'], i['mileage'])
+        except KeyError:
+            continue
+        print(i)
+        my_cars.append(i)
+
+counter = 0
+for i in my_cars:
+    counter += i['price']
+print(f'average cos - {int(counter / len(my_cars))}')
